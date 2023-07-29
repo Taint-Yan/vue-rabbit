cd3cb2f7317db93df68b4836d010f3befd1668f1
Taint-Yan <yantiti1@163.com>
2023-07-29 18:23:58 +0800

    完成主题颜色的定制

--- vite.config.js
+++ vite.config.js
@@ -16,12 +16,23 @@ export default defineConfig({
       resolvers: [ElementPlusResolver()],
     }),
     Components({
-      resolvers: [ElementPlusResolver()],
+      resolvers: [
+        // 1.配置element-plus采用sass样式配色系统
+        ElementPlusResolver({ importStyle: 'sass' })
+      ],
     }),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // 自动导入定制化样式文件进行样式覆盖
+        additionalData: `@use "@/styles/element/index.scss" as *;`,
+      }
     }
   }
 })
