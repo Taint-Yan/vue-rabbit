0c0e18c3a6cc2d55ee692c95df4c1362aebec288
Taint-Yan <yantiti1@163.com>
2023-07-30 15:00:25 +0800

    Layout静态页面搭建

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,6 @@
 <template>
   <!-- 路由占位符 -->
   <router-view />
-  <div class="test">自动导入样式</div>
 </template>
 
-<style scoped lang="scss">
-.test {
-  color: $priceColor;
-}
-</style>
+<style scoped lang="scss"></style>
--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,8 +1,14 @@
 <template>
-  <div>我是首页</div>
+  <LayoutNav />
+  <LayoutHeader />
   <router-view></router-view>
+  <LayoutFooter />
 </template>
 
-<script setup></script>
+<script setup>
+import LayoutNav from './components/LayoutNav.vue'
+import LayoutFooter from './components/LayoutFooter.vue'
+import LayoutHeader from './components/LayoutHeader.vue'
+</script>
 
 <style lang="scss" scoped></style>
