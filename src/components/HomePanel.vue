0a75b055ffb999956941e39f1492cfb9475e58c7
Taint-Yan <yantiti1@163.com>
2023-07-30 22:26:21 +0800

    Home面板组件封装

---
+++ src/components/HomePanel.vue
@@ -0,0 +1,59 @@
+<!-- 封装面板组件 -->
+<template>
+  <div class="home-panel">
+    <div class="container">
+      <div class="head">
+        <!-- 主标题和副标题 -->
+        <h3>
+          {{ title }}<small>{{ subTitle }}</small>
+        </h3>
+      </div>
+      <!-- 主体内容区域  使用插槽 -->
+      <slot class="main"></slot>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+
+defineProps({
+  title: {
+    type: String,
+    default: ''
+  },
+  subTitle: {
+    type: String,
+    default: ''
+  }
+})
+
+// 标题使用props接受
+</script>
+
+<style lang="scss" scoped>
+.home-panel {
+  background-color: #fff;
+
+  .head {
+    padding: 40px 0;
+    display: flex;
+    align-items: flex-end;
+
+    h3 {
+      flex: 1;
+      font-size: 32px;
+      font-weight: normal;
+      margin-left: 6px;
+      height: 35px;
+      line-height: 35px;
+
+      small {
+        font-size: 16px;
+        color: #999;
+        margin-left: 20px;
+      }
+    }
+  }
+}
+</style>
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -6,6 +6,12 @@
   <HomeNew />
   <HomeHot />
   <homeProduct />
+  <HomePanel title="新鲜好物 好多商品" sub-title="新鲜好物 好多商品">
+    <div>内容区</div>
+  </HomePanel>
+  <HomePanel title="人气推荐" sub-title="人气推荐 好多商品">
+    <div>内容区</div>
+  </HomePanel>
 </template>
 
 <script setup>
@@ -14,6 +20,7 @@ import HomeBanner from './components/HomeBanner.vue'
 import HomeNew from './components/HomeNew.vue'
 import HomeHot from './components/HomeHot.vue'
 import homeProduct from './components/HomeProduct.vue'
+import HomePanel from '@/components/HomePanel.vue'
 </script>
 
 <style lang="scss" scoped></style>
