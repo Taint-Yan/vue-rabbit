24577820b36a944766af1b995661279a53b2f990
Taint-Yan <yantiti1@163.com>
2023-07-30 19:17:28 +0800

    实现banner轮播图

---
+++ src/apis/home.js
@@ -0,0 +1,12 @@
+import httpInstance from "@/utils/http";
+
+
+/**
+ *  获取banner
+ */
+
+export function getBannerApi() {
+  return httpInstance({
+    url: 'home/banner'
+  });
+}
--- src/views/Home/components/HomeBanner.vue
+++ src/views/Home/components/HomeBanner.vue
@@ -1,7 +1,40 @@
 <template>
-  <div>我是HomeBanner页面</div>
+  <div class="home-banner">
+    <el-carousel height="500px">
+      <el-carousel-item v-for="item in bannerList" :key="item.id">
+        <img :src="item.imgUrl" alt="" />
+      </el-carousel-item>
+    </el-carousel>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref, onMounted } from 'vue'
+import { getBannerApi } from '@/apis/home.js'
 
-<style lang="scss" scoped></style>
+const bannerList = ref([])
+const getBanner = async () => {
+  const data = await getBannerApi()
+  bannerList.value = data.result
+}
+
+onMounted(() => {
+  getBanner()
+})
+</script>
+
+<style lang="scss" scoped>
+.home-banner {
+  width: 1240px;
+  height: 500px;
+  position: absolute;
+  left: 0;
+  top: 0;
+  z-index: 98;
+
+  img {
+    width: 100%;
+    height: 500px;
+  }
+}
+</style>
