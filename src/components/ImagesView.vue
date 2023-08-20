56430517442e877cedbdf021d67285edaf269dd4
Taint-Yan <yantiti1@163.com>
2023-08-20 17:20:03 +0800

    放大镜效果-鼠标跟随移动

--- src/components/ImagesView.vue
+++ src/components/ImagesView.vue
@@ -4,7 +4,7 @@
     <div class="middle" ref="target">
       <img :src="imageList[curIndex]" alt="" />
       <!-- 蒙层小滑块 -->
-      <div class="layer" :style="{ left: `0px`, top: `0px` }"></div>
+      <div class="layer" v-show="!isOutside" :style="{ left: `${left}px`, top: `${top}px` }"></div>
     </div>
     <!-- 小图列表 -->
     <ul class="small">
@@ -23,8 +23,8 @@
       :style="[
         {
           backgroundImage: `url(${imageList[0]})`,
-          backgroundPositionX: `0px`,
-          backgroundPositionY: `0px`
+          backgroundPositionX: `${positionX}px`,
+          backgroundPositionY: `${positionY}px`
         }
       ]"
       v-show="false"
@@ -33,7 +33,8 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
+import { useMouseInElement } from '@vueuse/core'
 // 图片列表
 const imageList = [
   'https://yanxuan-item.nosdn.127.net/d917c92e663c5ed0bb577c7ded73e4ec.png',
@@ -43,7 +44,7 @@ const imageList = [
   'https://yanxuan-item.nosdn.127.net/f881cfe7de9a576aaeea6ee0d1d24823.jpg'
 ]
 
-// 默认显示
+// 小图切换大图显示默认显示
 const curIndex = ref(0)
 
 // 鼠标移入
@@ -51,6 +52,50 @@ const MouseenTer = (i) => {
   curIndex.value = i
   console.log(i)
 }
+// 获取鼠标相对位置
+const target = ref(null)
+//useMouseInElement 中获取 X、Ｙ的值
+const { elementX, elementY, isOutside } = useMouseInElement(target)
+
+// 3. 控制滑块跟随鼠标移动（监听elementX/Y变化，一旦变化 重新设置left/top）
+const left = ref(0)
+const top = ref(0)
+
+const positionX = ref(0)
+const positionY = ref(0)
+watch([elementX, elementY], () => {
+  // 如果鼠标没有移入盒子直接不执行
+  if (isOutside.value) return
+
+  // 有效范围滑动距离
+  // 纵向
+  if (elementX.value > 100 && elementX.value < 300) {
+    left.value = elementX.value - 100
+  }
+
+  // 横向
+  if (elementY.value > 100 && elementY.value < 300) {
+    top.value = elementY.value - 100
+  }
+
+  // 边界
+  if (elementX.value > 300) {
+    left.value = 200
+  }
+  if (elementX.value < 100) {
+    left.value = 0
+  }
+  if (elementY.value > 300) {
+    top.value = 200
+  }
+  if (elementY.value < 100) {
+    top.value = 0
+  }
+
+  // 控制大图显示
+  positionX.value = -left.value * 2
+  positionY.value = -top.value * 2
+})
 </script>
 
 <style lang="scss" scoped>
